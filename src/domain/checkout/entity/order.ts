9bb0cf6a0bb6f1cebf10271fc102b48cb1279515
Sergio Kopplin <sergiokopplin@gmail.com>
2022-08-02 18:52:41 -0300

    feat: adds order item on order

--- src/domain/checkout/entity/order.ts
+++ src/domain/checkout/entity/order.ts
@@ -1,6 +1,8 @@
+import { OrderItem } from '../value-object/order-item';
+
 export class Order {
   private id: string;
   private customerId: string;
-  private items: any;
+  private items: OrderItem[];
   private total: number;
 }

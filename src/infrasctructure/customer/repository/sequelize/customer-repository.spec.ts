14796697b6d53e2f0a2d6f2bda2007cf3e5f278d
Sergio Kopplin <sergiokopplin@gmail.com>
2022-08-15 19:11:16 -0300

    feat: ensure customer repository update

--- src/infrasctructure/customer/repository/sequelize/customer-repository.spec.ts
+++ src/infrasctructure/customer/repository/sequelize/customer-repository.spec.ts
@@ -5,6 +5,23 @@ import { Sequelize } from 'sequelize-typescript'
 describe('Customer Repository', () => {
   let sequelize: Sequelize
 
+  const fakeId = 'c580a27d-8a17-465d-9030-4d83e41a622c'
+
+  const fakeAddress = makeAddressSut({
+    city: 'fake_city',
+    number: 1234,
+    street: 'fake_street',
+    zip: 'fake_zip'
+  }).sut
+
+  const fakeCustomer = makeCustomerSut({
+    address: fakeAddress,
+    active: false,
+    id: fakeId,
+    name: 'fake_name',
+    rewardPoints: 100
+  }).sut
+
   beforeEach(async () => {
     sequelize = new Sequelize({
       dialect: 'sqlite',
@@ -22,23 +39,10 @@ describe('Customer Repository', () => {
   })
 
   test('Should create', async () => {
-    const fakeId = 'c580a27d-8a17-465d-9030-4d83e41a622c'
     const sut = new CustomerRepository()
-    const fakeAddress = makeAddressSut({
-      city: 'fake_city',
-      number: 1234,
-      street: 'fake_street',
-      zip: 'fake_zip'
-    }).sut
-    const fakeCustomer = makeCustomerSut({
-      address: fakeAddress,
-      active: false,
-      id: fakeId,
-      name: 'fake_name',
-      rewardPoints: 100
-    }).sut
 
     expect(await CustomerModel.findAll()).toEqual([])
+
     await sut.create(fakeCustomer)
     const resultFindOne = await CustomerModel.findOne({ where: { id: fakeId } })
 
@@ -47,6 +51,30 @@ describe('Customer Repository', () => {
       city: fakeAddress.city,
       id: fakeId,
       name: fakeCustomer.name,
+      number: fakeAddress.number,
+      rewardPoints: fakeCustomer.rewardPoints,
+      street: fakeAddress.street,
+      zipcode: fakeAddress.zip
+    })
+  })
+
+  test('Should update', async () => {
+    const localFakeCustomer = fakeCustomer
+    const fakeName = 'fake_name_2'
+    const sut = new CustomerRepository()
+
+    await sut.create(localFakeCustomer)
+
+    localFakeCustomer.changeName(fakeName)
+
+    await sut.update(fakeCustomer)
+
+    const resultFindOne2 = await CustomerModel.findOne({ where: { id: fakeId } })
+    expect(resultFindOne2.toJSON()).toEqual({
+      active: false,
+      city: fakeAddress.city,
+      id: fakeId,
+      name: fakeName,
       number: fakeAddress.number,
       rewardPoints: fakeCustomer.rewardPoints,
       street: fakeAddress.street,
--- src/infrasctructure/customer/repository/sequelize/customer-repository.ts
+++ src/infrasctructure/customer/repository/sequelize/customer-repository.ts
@@ -3,6 +3,9 @@ import { Customer, CustomerRepositoryInterface } from 'domain/customer'
 import { CustomerModel } from './customer-model'
 
 export class CustomerRepository implements CustomerRepositoryInterface {
+  find: (id: string) => Promise<Customer>
+  findAll: () => Promise<Customer[]>
+
   async create (entity: Customer): Promise<void> {
     await CustomerModel.create({
       id: entity.id,
@@ -16,7 +19,20 @@ export class CustomerRepository implements CustomerRepositoryInterface {
     })
   }
 
-  update: (entity: Customer) => Promise<void>
-  find: (id: string) => Promise<Customer>
-  findAll: () => Promise<Customer[]>
+  async update (entity: Customer): Promise<void> {
+    await CustomerModel.update({
+      id: entity.id,
+      name: entity.name,
+      street: entity.address.street,
+      number: entity.address.number,
+      zipcode: entity.address.zip,
+      city: entity.address.city,
+      active: entity.isActive(),
+      rewardPoints: entity.rewardPoints
+    }, {
+      where: {
+        id: entity.id
+      }
+    })
+  }
 }
